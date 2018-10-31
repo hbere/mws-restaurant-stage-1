9f074488187e06c8674d7b662ab0f1b749b22acb
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-31 14:55:31 -0600

    Updates service worker

--- sw.js
+++ sw.js
@@ -7,50 +7,77 @@
 // "A real service worker implementation would use caching and onfetch rather than the deprecated XMLHttpRequest API. Those features are not used here so that you can focus on understanding Promises."
 
 let STATIC_CACHE = 'restaurant-static-v1';
+let HOME_URL = 'http://localhost:8000/'
 
 // Start caching
 // Event fires when service worker installed for the first time
 self.addEventListener('install', event => {
     // Confirm the event happened
     console.log('Service worker was installed: ', event);
-    // Cache all local requests
-    event.waitUntil(
-        caches.open(STATIC_CACHE).then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/img/2.jpg',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/data/restaurants.json'
-            ]);
-        })
-    );
+
+    // // TODO Figure out how to add entire folders at a time
+    // // Cache a list of items
+    // event.waitUntil(
+    //     caches.open(STATIC_CACHE).then(function (cache) {
+    //         return cache.addAll([
+    //             '/',
+    //             '/img/2.jpg',
+    //             '/js/dbhelper.js',
+    //             '/js/main.js',
+    //             '/js/restaurant_info.js',
+    //             '/data/restaurants.json'
+    //         ]);
+    //     })
+    // );
 });
 
 // Delete old cache (after new one is in use)
 // Event fires when service worker activated & ready for use
-self.addEventListener('activate', function(event) {
-    // event.waitUntil(
-    //     caches.delete('restaurant-static-v0')
-    // );
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('restaurant-static') && cacheName != STATIC_CACHE;
-                }).map(function(cacheName) {
-                    return cache.delete(cacheName);
-                })
-            );
-        })
+        caches.delete('restaurant-static-v0')
     );
+
+    // // TODO fix the following code if ever need to make this process more sustainable
+    // event.waitUntil(
+    //     caches.keys().then(function (cacheNames) {
+    //         return Promise.all(
+    //             cacheNames.filter(function (cacheName) {
+    //                 return cacheName.startsWith('restaurant-static') && cacheName != STATIC_CACHE;
+    //             }).map(function (cacheName) {
+    //                 return cache.delete(cacheName);
+    //             })
+    //         );
+    //     })
+    // );
 });
 
 self.addEventListener('fetch', event => {
 
     // Confirm the event happened
     console.log('Fetch event occurred: ', event.request.url);
+
+    // Load from cache if already available, otherwise save to cache
+    // TODO figure out how to handle better pages like http://localhost:8000/restaurant.html?id=2
+        // if loaded without caching first
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                // Return the item if it is already there
+                // console.log('2', event.request.url);
+                return response;
+            } else if (event.request.url.startsWith(HOME_URL)) {
+                // If not, cache the item
+                // console.log('3', event.request.url);
+                caches.open(STATIC_CACHE).then(function (cache) {
+                    return cache.add(event.request.url);
+                })
+            }
+            return fetch(event.request);
+        }).catch(function (e) {
+            console.log(e);
+        })
+    );
 
     // // Example HTML response
     // let html = '<html>It\'s <strong class="a winner is me">me!</strong></html>';
@@ -81,25 +108,5 @@ self.addEventListener('fetch', event => {
     //         return new Response("Uh oh, that totally failed");
     //     })
     // );
-
-    // Example #4: load from cache if already available
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) return response;
-            return fetch(event.request);
-        })
-    );
-
-    // // Define variables
-    // let requestedURL = event.request.url;
-    // let windowURLRegex = new RegExp(`^${window.location.href}(.+)`);
-
-    // // Give initial feedback
-    // console.log('Fetch event occurred: ', requestedURL);
-
-    // // Cache the file if requestedURL begins with windowURL
-    // if (requestedURL == windowURLRegex) {
-    //     console.log("local");
-    // }
 
 });
