cbaee4bf0b247b2b96d72d894d62398838434946
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-31 11:27:33 -0600

    Getting started with a service worker

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+// Service Worker to cache files already visited
+// Remember service worker default scope = its location
+self.addEventListener('install', event => {
+    console.log('service worker was installed');
+});
