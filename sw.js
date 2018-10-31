0183ca88aaddc091d3928004ce2f0eb41788c4c9
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-31 11:55:07 -0600

    Updates service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 
+// TODO adjust service worker scope and move this file to js folder
+
 // Service Worker to cache files already visited
 // Remember service worker default scope = its location
 self.addEventListener('install', event => {
