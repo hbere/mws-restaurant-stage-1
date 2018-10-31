311b6063d004c0f8b72ab0b5b082fdc851ba36d5
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-31 12:10:36 -0600

    Updates service worker

--- sw.js
+++ sw.js
@@ -4,5 +4,9 @@
 // Service Worker to cache files already visited
 // Remember service worker default scope = its location
 self.addEventListener('install', event => {
-    console.log('Service worker was installed');
+    console.log('Service worker was installed: ', event);
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event occurred: ', event);
 });
